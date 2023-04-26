2c3511dae1d4450bdac3b4b5b02745359b9d8e7d
Deathsml4 <124763973+Deathsml4@users.noreply.github.com>
2023-04-26 14:26:59 +0700

    Final Update

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include <ctime>
+#include <fstream>
 #include "game.h"
 #include "TextObject.h"
 
@@ -81,10 +82,15 @@ struct point {
 };
 
 struct Laser{
+    Uint32 time_;
+    Uint32 getTime(){
+        return SDL_GetTicks();
+    }
     int size = 3;
+    int step = 15;
     int x[6],y[6];
     bool gameOver = 0;
-    void init (){
+    void init1 (){
          x[0] = - x_central ; y[0] = -y_central; // Tâm màn hình tại [0,0]
          x[1] = - SCREEN_HEIGHT / 2.0 ; y[1] = - x_central;
          x[2] = 0 ; y[2] = - x_central;
@@ -93,7 +99,71 @@ struct Laser{
          x[5] = x_central ; y[5] = 0;
     }
     int side = 1;
+    /*SDL_Rect getRect (int x_, int y_){
+        SDL_Rect rect;
+        rect.h = 500;
+        rect.w = 500;
+        rect.x = x_;
+        rect.y =y_;
+        return rect;
+    }*/
+    /*SDL_Rect rectgame2 = getRect(100,100);*/
+    SDL_Rect* threat;
+    int move = 0;
+    bool reached = 0;
+    void move2 (){
+        if (reached || SDL_GetTicks() - time_ == 100){
+            int a = rand()%4;
+            while (a == move){
+                a = rand()%4;
+            }
+            move = a;
+            reached = 0;
+            time_ = getTime();
+        }
+        switch (move){
+            case 0: moveUp(threat);  break;
+            case 1: moveDown(threat); break;
+            case 2: moveLeft(threat) ; break;
+            case 3: moveRight(threat) ; break;
+        }
+    }
+    void render2(SDL_Renderer* renderer, SDL_Rect* player,bool game2){
+        /*SDL_SetRenderDrawColor(renderer,255,255,120,1);
+        SDL_RenderFillRect(renderer,threat);*/
 
+        if (inside2(player) && game2){
+            gameOver = 1;
+        }
+    }
+    void moveUp (SDL_Rect* prect){
+        if (prect->y > step + 30) prect->y -= step;
+        else reached = 1;
+    }
+    void moveDown (SDL_Rect* prect){
+        if (prect->y + prect->h < SCREEN_HEIGHT - step - 30) prect->y += step;
+        else reached = 1;
+    }
+    void moveLeft (SDL_Rect* prect){
+        if (prect->x > step + 30) prect->x -= step;
+        else reached = 1;
+    }
+    void moveRight (SDL_Rect* prect){
+        if (prect->x + prect->w < SCREEN_WIDTH - step - 30) prect->x += step;
+        else reached = 1;
+    }
+    /*bool inside1(SDL_Rect* prect){
+        if (prect->x <= 100 || prect->x >= SCREEN_WIDTH - 100) return 0;
+        if (prect->y <= 100 || prect->y >= SCREEN_HEIGHT - 100) return 0;
+        return 1;
+    }*/
+    bool inside2(SDL_Rect* prect1){
+        if (prect1->x <= threat->x || prect1->x >= threat->x + threat->w) return 0;
+        if (prect1->y <= threat->y || prect1->y >= threat->y + threat->h) return 0;
+        if (prect1->x + prect1->w <= threat->x || prect1->x + prect1->w >= threat->x + threat->w) return 0;
+        if (prect1->y + prect1->h <= threat->y || prect1->y + prect1->h >= threat->y + threat->h) return 0;
+        return 1;
+    }
     void roll(SDL_Rect* prect){
         for (int i = 0 ; i < 6 ; i++){
             int new_x = x[i]*0.9999 - y[i]*0.0141*side;
@@ -215,7 +285,7 @@ int main(int argc, char* argv[])
     SDL_Event event;
     printMenu(renderer,font);
     SDL_RenderPresent(renderer);
-    SDL_Delay(5000);
+    SDL_Delay(3000);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
     SDL_Texture* GameOver = loadTexture("GameOver.png", renderer);
@@ -237,17 +307,22 @@ int main(int argc, char* argv[])
 
 
 
-
     SDL_RenderPresent(renderer);
 
+    SDL_Texture* pic2 = loadTexture("slime.png", renderer);
+    SDL_Rect rect2;
 
-
+    SDL_QueryTexture (pic, NULL, NULL, &rect2.w, &rect2.h);
+    rect2.x = 100;
+    rect2.y = 100;
 
 
     Laser laser;
-    laser.init();
+    laser.init1();
     point Point;
     Point.get_point();
+
+    laser.threat = &rect2;
 
     Uint32 time = SDL_GetTicks();
 
@@ -255,11 +330,13 @@ int main(int argc, char* argv[])
 
     bool quit = false;
     bool paused = 0;
-
+    bool exit = 0;
     bool game1 = 0;
     bool game1_done = 0;
     bool game2 = 0;
+    int oldScore = 0;
     while (!quit){
+
         if (SDL_PollEvent(&event) == 0) {
             continue;
         }
@@ -267,9 +344,13 @@ int main(int argc, char* argv[])
         else if (event.type == SDL_KEYDOWN){
             switch (event.key.keysym.sym){
                 case SDLK_ESCAPE: quit = true; break;
-                case SDLK_SPACE:
-                    game1 = 1;
+                case SDLK_SPACE:{
+                    if (game1_done){
+                        game2 = 1;
+                    }
+                    else game1 = 1;
                     break;
+                }
                 default : break;
             }
         }
@@ -279,10 +360,11 @@ int main(int argc, char* argv[])
             rect.x = x_mouse - rect.w/2;
             rect.y = y_mouse - rect.h/2;
         }
+        //if (!game1_done) background = loadTexture("Background 2.jpeg", renderer);
         SDL_RenderCopy(renderer, background, NULL, NULL);
         SDL_RenderCopy(renderer, pic, NULL, &rect);
         SDL_RenderPresent(renderer);
-        laser.render(renderer);
+        if (!game1_done) laser.render(renderer);
         SDL_RenderPresent(renderer);
         while (game1){
             if (paused){
@@ -300,6 +382,7 @@ int main(int argc, char* argv[])
                 }
                 continue;
             }
+
             if (SDL_GetTicks() - time  >= 30000){
                 laser.side *= -1;
                 time = SDL_GetTicks();
@@ -312,8 +395,7 @@ int main(int argc, char* argv[])
             SDL_RenderCopy(renderer, background, NULL, NULL);
             SDL_RenderCopy(renderer, pic, NULL, &rect);
 
-
-            laser.render(renderer);
+            if (!game1_done) laser.render(renderer);
             Point.render(renderer,&rect);
             if (SDL_GetTicks() - time  >= 28000){
                 red_text.setText(str_switch);
@@ -368,6 +450,109 @@ int main(int argc, char* argv[])
                 SDL_RenderPresent(renderer);
                 quit = 1;
                 game1 = 0;
+            }
+            if (Point.score - oldScore == 3){
+                game1_done = 1;
+                game1 = 0;
+                oldScore = Point.score;
+            }
+
+        }
+        if (game1_done) {
+            SDL_RenderCopy(renderer, pic2, NULL, laser.threat);
+            laser.render2(renderer,&rect,game2);
+            SDL_RenderPresent(renderer);
+            laser.time_ = laser.getTime();
+        }
+        while (game2){
+            if (paused){
+                if (SDL_PollEvent(&event) == 0) {
+                    continue;
+                }
+                if (event.type == SDL_KEYDOWN){
+                    if (event.key.keysym.sym == SDLK_p){
+                        paused = !paused;
+                    }
+                    else if (event.key.keysym.sym == SDLK_ESCAPE){
+                        game2 = 0;
+                        quit = 1;
+                    }
+                }
+                continue;
+            }
+            if (SDL_GetTicks() - time  >= 30000){
+                laser.side *= -1;
+                time = SDL_GetTicks();
+            }
+
+            laser.move2();
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderClear(renderer);
+
+            SDL_RenderCopy(renderer, background, NULL, NULL);
+            SDL_RenderCopy(renderer, pic, NULL, &rect);
+
+            SDL_RenderCopy(renderer, pic2, NULL, laser.threat);
+            laser.render2(renderer,&rect,game2);
+            Point.render(renderer,&rect);
+            /*if (SDL_GetTicks() - time  >= 28000){
+                red_text.setText(str_switch);
+                red_text.loadFromRenderText(font, renderer);
+                red_text.render(renderer,SCREEN_WIDTH/2 - 200, SCREEN_HEIGHT - 200);
+                SDL_RenderPresent(renderer);
+                //SDL_Delay(50);
+
+            }*/
+
+            //Show score
+            std::string str_score = "Score: ";
+            std::string str_val = std::to_string(Point.score);
+            str_score += str_val;
+
+            score_text.setText(str_score);
+            score_text.loadFromRenderText(font, renderer);
+            score_text.render(renderer,SCREEN_WIDTH - 200, 15);
+
+            SDL_RenderPresent(renderer);
+
+            if (SDL_PollEvent(&event) == 0) {
+                SDL_Delay(50);
+                continue;
+            }
+            else if (event.type == SDL_QUIT) break;
+            else if (event.type == SDL_KEYDOWN){
+                switch (event.key.keysym.sym){
+                    case SDLK_ESCAPE: quit = 1; game2 = 0 ; break;
+                    case SDLK_p:{
+                        paused = !paused;
+                        break;
+                    }
+                    default : break;
+                }
+            }
+            else if (event.type == SDL_MOUSEMOTION){
+                int x_mouse,y_mouse;
+                SDL_GetMouseState (&x_mouse,&y_mouse);
+                rect.x = x_mouse - rect.w/2;
+                rect.y = y_mouse - rect.h/2;
+            }
+            if (laser.gameOver) {
+                SDL_RenderCopy(renderer, GameOver, NULL, NULL);
+
+                font = TTF_OpenFont("Font//Calibri Light.ttf",60);
+                black_text.setText(str_score);
+                black_text.loadFromRenderText(font, renderer);
+                black_text.render(renderer,SCREEN_WIDTH/2 -100, SCREEN_HEIGHT -200);
+                quit = 1;
+
+                SDL_RenderPresent(renderer);
+                quit = 1;
+                game2 = 0;
+            }
+            if (Point.score - oldScore == 3){
+                game2 = 0;
+                game1_done = 0;
+                oldScore = Point.score;
             }
 
         }
